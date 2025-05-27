9bb8eea851d2db95c42df6159cc62285903dbad9
SUBHAM <131864464+MSubham06@users.noreply.github.com>
2025-05-28 00:06:50 +0530

    Add files via upload

--- m11.java
+++ m11.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+public class m11 {
+    public static void printhello() {
+        System.out.println("Hello World");
+        return;
+    }
+    public static void main(String[] args) {
+        printhello(); // fuction call
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

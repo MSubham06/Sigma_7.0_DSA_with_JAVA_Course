071de2dbcd0dbb39e70560242116e67be31daf45
SUBHAM <131864464+MSubham06@users.noreply.github.com>
2025-05-28 18:47:08 +0530

    Add files via upload

--- m11.java
+++ m11.java
@@ -1,29 +1,114 @@
 import java.util.*;
 
 public class m11 {
-    public static void printhello() {
-        System.out.println("Hello World");
-        return;
+    // public static void printhello() {
+    //     System.out.println("Hello World");
+    //     return;
+    // }
+    // public static void calculateSum(int a, int b) {
+    //     // Scanner sc = new Scanner (System.in);
+    //     // int a = sc.nextInt();
+    //     // int b = sc.nextInt();
+    //     int sum = a + b;
+    //     System.out.println("Sum is " + sum);
+    //     return;
+    // }
+    // public static void main(String args[]) {
+    //     //printhello(); // fuction call
+    //     Scanner sc = new Scanner (System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     calculateSum(a,b);
+    //     System.out.println("Sum is " + sum);
+    // }
+
+
+    // public static void swap(int a,int b) {
+    //     //swap
+    //     int temp = a;
+    //     a = b;
+    //     b = temp;
+    //     System.out.println("a is " + a +" b is " + b);
+    // }
+    // public static void main(String[] args) {
+    //     //swap - values exchage 
+    //     int a = 5;
+    //     int b = 10;
+    //     swap(a,b);
+    // }
+
+    // public static int multiply(int a,int b) {
+    //     int product = a * b;
+    //     return product;
+    // }
+    // public static void main(String args[]) {
+    //     int a = 3;
+    //     int b = 5;
+    //     int prod = multiply(a,b);
+    //     System.out.println("Product is " + prod);
+    //     prod = multiply(4,6);
+    //     System.out.println("Product is " + prod);
+    // }
+
+    public static int factorial(int n) {
+        int f = 1;
+        
+        for(int i=1; i<=n; i++) {
+            f = f * i;
+        }
+        
+        return f;
+    }
+
+   // for factorial 
+    // public static void main(String args[]) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter the number : ");
+    //     int n = sc.nextInt();
+    //     int fact = factorial(n);
+    //     System.out.println("Factorial of " + n + " is " + fact);
+    // }
+ 
+    public static int binCoeff(int n, int r) {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+
+        int binCoeff = fact_n / (fact_r * fact_nmr);
+        return binCoeff;
+    }
+
+    // public static void main(String[] args) {
+    //     System.out.println("Binomial Coefficient of 5C2 is " + binCoeff(5, 2));
+    // }
+    
+
+
+
+    //Fuction overloading using parameters
+    // public static int sum(int a, int b) {
+    //     return a + b;
+    // }
+    // public static int sum(int a, int b, int c) {
+    //     return a + b + c;
+    // }
+    // public static void main(String[] args) {
+    //     System.out.println("Sum of 2 numbers is " + sum(5, 10));
+    //     System.out.println("Sum of 3 numbers is " + sum(5, 10, 20));
+    // }
+
+    //Fuction overloading using Data Types
+    public static int sum(int a, int b) {
+        return a+b;
+    } 
+    public static float sum(float a, float b) {
+        return a+b;
     }
     public static void main(String[] args) {
-        printhello(); // fuction call
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Sum of 2 numbers is " + sum(5, 10));
+        System.out.println("Sum of 3 numbers is " + sum(5.2f,4.5f));
     }
+
+
+
 }
